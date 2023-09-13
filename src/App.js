496574695a38497b52d08ca8d1fbdd93480efe5c
kurra chandu <nanic6465@gmail.com>
2023-09-13 18:53:52 +0530

    Displaying the piechart in center

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import PieChart from './components/PieChart';
 
 function App() {
   const slice_data = [
-    {slice_name: "Apple", slice_percentage: 10 },
-    {slice_name: "Redme", slice_percentage: 20 },
-    {slice_name: "Jio", slice_percentage: 30 },
-    {slice_name: "LG", slice_percentage: 15 },
-    {slice_name: "Moto", slice_percentage: 25 }
+    { slice_name: "Apple", slice_percentage: 10, slice_Colors: 'ff5733' },
+    { slice_name: "Redme", slice_percentage: 20, slice_Colors: '33ff57' },
+    { slice_name: "Jio", slice_percentage: 20, slice_Colors: '5733ff' },
+    { slice_name: "LG", slice_percentage: 15, slice_Colors: 'ffff33' },
+    { slice_name: "Moto", slice_percentage: 25, slice_Colors: 'ff33ff' },
   ];
   return (
     <div className="App">
       <h1>Pie Chart Example</h1>
-      <PieChart slice_data={slice_data} />
+      <div className="center-container">
+        <PieChart slice_data={slice_data} />
+      </div>
     </div>
   );
 }
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,42 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './PieChart.css';
 
 const PieChart = ({ slice_data }) => {
-  const [slicesData, setSlicesData] = useState([]);
-  const [temp, setTemp] = useState(0);
+  const totalPercentage = slice_data.reduce((total, slice) => total + parseFloat(slice.slice_percentage), 0);
 
-  useEffect(() => {
-    const totalPercentage = slice_data.reduce((total, slice) => total + parseFloat(slice.slice_percentage), 0);
-    const fixedAngle = 360 / slice_data.length;
-
-    const slicesData = slice_data.map((slice, index) => ({
-      name: slice.slice_name,
-      percentage: parseFloat(slice.slice_percentage),
-      angle: temp + index * fixedAngle, // Calculate the angle based on index
-    }));
-
-    setSlicesData(slicesData);
-  }, [slice_data, temp]);
+  let startAngle = 0;
 
   return (
     <div className="pie-chart-container">
-      <div className="pie-chart">
-        {slicesData.map((slice, index) => (
-          <div
-            key={index}
-            className="slice"
-            style={{
-              transform: `rotate(${slice.angle}deg)`, // Use slice.angle here
-            }}
-          >
-            <span className="label">
-              {slice.name}
-              <br />
-              <span className="percentage">{slice.percentage}%</span>
-            </span>
-          </div>
-        ))}
-      </div>
+      <svg viewBox="0 0 250 250" className="pie-chart"> {/* Adjust the viewBox size */}
+        {slice_data.map((slice, index) => {
+          const percentage = parseFloat(slice.slice_percentage);
+          const sliceAngle = (percentage / totalPercentage) * 360;
+          const endAngle = startAngle + sliceAngle;
+          const largeArcFlag = sliceAngle > 180 ? 1 : 0;
+          //svg path data using commands based on start,end,& largearcFlag angles for displaying slice of piechart
+          const pathData = `
+            M 40 40
+            L ${40 + 40 * Math.cos((startAngle - 90) * (Math.PI / 180))} ${40 + 40 * Math.sin((startAngle - 90) * (Math.PI / 180))}
+            A 40 40 0 ${largeArcFlag} 1 ${40 + 40 * Math.cos((endAngle - 90) * (Math.PI / 180))} ${40 + 40 * Math.sin((endAngle - 90) * (Math.PI / 180))}
+            Z
+          `;
+
+          // Calculate the position for the text element for placing text label of slice
+          const textX = 40 + 30 * Math.cos(((startAngle + sliceAngle / 2) - 90) * (Math.PI / 180));
+          const textY = 40 + 30 * Math.sin(((startAngle + sliceAngle / 2) - 90) * (Math.PI / 180));
+
+          startAngle = endAngle;
+
+          return (
+            <g key={index}>
+              <path
+                d={pathData}
+                fill={`#${slice.slice_Colors}`} // Use slice_Colors property
+              />
+              {/* Create a text element for data display */}
+              <text x={textX} y={textY} fontSize="6px" fill="black">
+                {slice.slice_name} {slice.slice_percentage}%
+              </text>
+            </g>
+          );
+        })}
+      </svg>
     </div>
   );
 };
