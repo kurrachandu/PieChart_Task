53dcea1ae53a232d72783bdd446257d7b9bd117f
kurra chandu <nanic6465@gmail.com>
2023-09-14 15:57:59 +0530

    changes the code for displaying piechart properly

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const slice_data = [
     { slice_name: "Apple", slice_percentage: 10, slice_Colors: 'ff5733' },
     { slice_name: "Redme", slice_percentage: 20, slice_Colors: '33ff57' },
-    { slice_name: "Jio", slice_percentage: 20, slice_Colors: '5733ff' },
+    { slice_name: "Jio", slice_percentage: 30, slice_Colors: '5733ff' },
     { slice_name: "LG", slice_percentage: 15, slice_Colors: 'ffff33' },
     { slice_name: "Moto", slice_percentage: 25, slice_Colors: 'ff33ff' },
   ];
